967711a5eac0cb0737bb4d6e16e0df36ba4c8c84
s1awek <elvizky@gmail.com>
2016-11-11 18:52:55 +0100

    Version 1.0, all is working as intended.

--- script.js
+++ script.js
@@ -3,25 +3,25 @@ $(document).ready(function () {
     var user = '/users/';
     var channel = '/channels/';
     var stream = '/streams/';
-    var usersList = ['quickybaby', 'orzanel', 'ESL_SC2', 'OgamingSC2', 'cretetion', 'freecodecamp', 'storbeck', 'habathcx', 'RobotCaleb', 'noobs2ninjas', 'brunofin', 'comster404', 'none_existing_user'];
+    var usersList = ['quickybaby', 'orzanel', 'ESL_SC2', 'OgamingSC2', 'cretetion', 'freecodecamp', 'storbeck', 'habathcx', 'RobotCaleb', 'noobs2ninjas', 'brunofin', 'comster404', '404_feded'];
 
     function getData(parm, user_name) {
         function myUserFunction(json) {
-            //console.log(json);
+            console.log(json);
             if (json.status == '404') {
-                $('#streamers-list').append('<li class="clearfix" id="' + user_name + '"><div class="left-container"><a href="#" class="' + user_name + '"> <span class="name">' + user_name + '</span></a></div></li>');
-                $('#' + user_name + ' a').prepend('<img src="img/placeholder.png" / >');
+                $('#streamers-list').append('<li class="clearfix" id="' + user_name + '"><div class="left-container"><span class="name-404 name">' + user_name + '</span></div></li>');
+                $('#' + user_name + ' .left-container').prepend('<img src="img/placeholder.png" class="logo clearfix" alt="Logo placeholder" width="100" height="100" / >');
             } else {
-                $('#streamers-list').append('<li class="clearfix" id="' + user_name + '"><div class="left-container"><a href="#" class="' + user_name + '"> <span class="name">' + json.display_name + '</span></a></div></li>');
+                $('#streamers-list').append('<li class="clearfix" id="' + user_name + '"><div class="left-container"><a href="#" class="name ' + user_name + '">' + json.display_name + '</a></div></li>');
 
                 if (json.bio != null) {
                     //console.log(json._id);
-                    $('#' + user_name + ' .left-container').append('<p class="bio">Bio: ' + json.bio + '</p>');
+                    $('#' + user_name + ' .left-container').append('<p class="bio"><span class="highlight">Bio: </span>' + json.bio + '</p>');
                 }
                 if (json.logo != null) {
-                    $('#' + user_name + ' a').prepend('<img src="' + json.logo + '" / >');
+                    $('#' + user_name + ' .left-container').prepend('<a href="#" class="' + user_name + '"><img src="' + json.logo + '" class="logo clearfix" alt="Logo" width="100" height="100" / ></a>');
                 } else {
-                    $('#' + user_name + ' a').prepend('<img src="img/placeholder.png" / >');
+                    $('#' + user_name + ' .left-container').prepend('<img src="img/placeholder.png" class="logo clearfix" alt="Logo placeholder" width="100" height="100" / >');
                 }
             }
         }
@@ -33,14 +33,14 @@ $(document).ready(function () {
             });
             //console.log(json.stream);
             if (json.stream === null) {
-                $('#' + user_name).append('<div class="right-container">Stream offline</div>');
+                $('#' + user_name).append('<div class="right-container clearfix"><span class="icon-offline"></span>Offline</div>');
                 $('#' + user_name).addClass('offline');
             } else if (json.stream === undefined) {
-                $('#' + user_name).append('<div class="right-container">' + json.message + '</div>');
+                $('#' + user_name).append('<div class="right-container clearfix">' + json.message + '</div>');
                 $('#' + user_name).addClass('offline');
             } else {
-                $('#' + user_name).append('<div class="right-container"><a href="https://twitch.tv/' + user_name + '/" target="_blank"> Go to stream</a></div>');
-                $('#' + user_name + ' .left-container').append('<p class="stream-info">Stream info: ' + json.stream.game + '</p>');
+                $('#' + user_name).append('<div class="right-container clearfix"><span class="icon-online"></span>Online</div>');
+                $('#' + user_name + ' .left-container').append('<p class="stream-info"><span class="highlight">Game: </span>' + json.stream.game + '</p>');
                 $('#' + user_name).addClass('online');
 
             }
@@ -112,11 +112,11 @@ $(document).ready(function () {
 
             // If the list item does not contain the text phrase fade it out
             if ($(this).text().search(new RegExp(filter, 'i')) < 0) {
-                $(this).parent().parent().parent().fadeOut();
+                $(this).parent().parent().fadeOut();
 
                 // Show the list item if the phrase matches and increase the count by 1
             } else {
-                $(this).parent().parent().parent().fadeIn();
+                $(this).parent().parent().fadeIn();
                 count++;
             }
         });
