e1bdf2e1c8d510fc3a202f8c2265b89fa0e821c5
s1awek <elvizky@gmail.com>
2017-04-13 13:41:00 +0200

    Completely refactored code, removed some bugs, added comments to script.js file.

--- script.js
+++ script.js
@@ -1,83 +1,116 @@
+/*jslint browser: true*/
+/*global $, Audio, jQuery, alert, console*/
 $(document).ready(function () {
+    //TODO: add user input for custom streamers
     'use strict';
-    var baseUrl = 'https://wind-bow.hyperdev.space/twitch-api/';
-    var user = '/users/';
-    var channel = '/channels/';
-    var stream = '/streams/';
-    var usersList = ['quickybaby', 'orzanel', 'ESL_SC2', 'OgamingSC2', 'cretetion', 'freecodecamp', 'storbeck', 'habathcx', 'RobotCaleb', 'noobs2ninjas', 'brunofin', 'comster404', '404_feded'];
+    var baseUrl = 'https://wind-bow.hyperdev.space/twitch-api/',
+        i,
+        j,
+        usersList = ['quickybaby', 'orzanel', 'ESL_SC2', 'OgamingSC2', 'cretetion', 'freecodecamp', 'storbeck', 'habathcx', 'RobotCaleb', 'noobs2ninjas', 'brunofin', 'comster404', '404_feded'];
+    //Convert users names to lower case to avoid spelling mistakes and use json.name (always all lower case) as id for HTML elements
+    usersList.forEach(function (elem, index, array) {
+        elem = elem.toLowerCase();
+        array[index] = elem;
+    });
 
-    function getData(parm, user_name) {
-        function myUserFunction(json) {
-            if (json.status == '404') {
-                $('#streamers-list').append('<li class="clearfix" id="' + user_name + '"><div class="left-container"><span class="name-404 name">' + user_name + '</span></div></li>');
-                $('#' + user_name + ' .left-container').prepend('<img src="https://res.cloudinary.com/s1awek/image/upload/twitchtv/placeholder.png" class="logo clearfix" alt="Logo placeholder" width="100" height="100" / >');
-            } else {
-                $('#streamers-list').append('<li class="clearfix" id="' + user_name + '"><div class="left-container"><a href="#" class="name ' + user_name + '">' + json.display_name + '</a></div></li>');
+    function createList() {
+        var i;
+        //Create unordered list based on usersList array
+        for (i = 0; i < usersList.length; i = i + 1) {
+            $('#streamers-list').append('<li class="clearfix offline" id="' + usersList[i] + '"><div class="left-container"></div><div class="right-container clearfix"><span class="status-icon icon-offline"></span>Offline</div></li>');
+        }
+        //Append apropriate HTML markup
+        $('<a href="" class="a-logo" target="_blank"><img src="" class="logo clearfix" alt="Logo" width="100" height="100"></a><a href="" class="name" target="_blank"></a><p class="bio hidden"><span class="highlight">Bio: </span></p><p class="stream-info hidden"><span class="highlight">Game: </span></p>').appendTo('.left-container');
+    }
+    createList();
 
-                if (json.bio !== null) {
-                    //console.log(json._id);
-                    $('#' + user_name + ' .left-container').append('<p class="bio"><span class="highlight">Bio: </span>' + json.bio + '</p>');
-                }
-                if (json.logo !== null) {
-                    $('#' + user_name + ' .left-container').prepend('<a href="#" class="' + user_name + '"><img src="' + json.logo + '" class="logo clearfix" alt="Logo" width="100" height="100" / ></a>');
-                } else {
-                    $('#' + user_name + ' .left-container').prepend('<img src="https://res.cloudinary.com/s1awek/image/upload/twitchtv/placeholder.png" class="logo clearfix" alt="Logo placeholder" width="100" height="100" / >');
-                }
+    //Handle JSON response for users
+    function users(json) {
+        //Setting up some variables
+        var name = json.name,
+            displayName = json.display_name,
+            logo = json.logo,
+            bio = json.bio,
+            error = json.error,
+            nameError;
+        if (logo) {
+            logo = json.logo;
+        } else {
+            //Logo placeholder if not present in json.logo
+            logo = 'https://res.cloudinary.com/s1awek/image/upload/twitchtv/placeholder.png';
+        }
+        //If no errors...
+        if (!json.error) {
+            $('#' + name + ' .left-container .a-logo').addClass(name);
+            $('#' + name + ' .left-container .a-logo').attr('href', 'https://twitch.tv/' + name);
+            $('#' + name + ' .left-container a img.logo').attr('src', logo);
+            $('#' + name + ' .left-container a.name').addClass(name);
+            $('#' + name + ' .left-container a.name').attr('href', 'https://twitch.tv/' + name);
+            $('#' + name + ' .left-container a.name').text(displayName);
+            if (bio) {
+                $('#' + name + ' .bio').removeClass('hidden');
+                $('#' + name + ' .bio').append(bio);
             }
-        }
-
-        function myStreamFunction(json) {
-            $('a.' + user_name).attr({
-                'href': 'https://twitch.tv/' + user_name,
-                'target': '_blank'
-            });
-            //console.log(json.stream);
-            if (json.stream === null) {
-                $('#' + user_name).append('<div class="right-container clearfix"><span class="icon-offline"></span>Offline</div>');
-                $('#' + user_name).addClass('offline');
-            } else if (json.stream === undefined) {
-                $('#' + user_name).append('<div class="right-container clearfix">' + json.message + '</div>');
-                $('#' + user_name).addClass('offline');
-            } else {
-                $('#' + user_name).append('<div class="right-container clearfix"><span class="icon-online"></span>Online</div>');
-                $('#' + user_name + ' .left-container').append('<p class="stream-info"><span class="highlight">Game: </span>' + json.stream.game + '</p>');
-                $('#' + user_name).addClass('online');
-
-            }
-        }
-
-        if (parm === user) {
-            $.ajax({
-                type: 'GET',
-                url: 'https://wind-bow.hyperdev.space/twitch-api' + parm + user_name,
-                dataType: 'jsonp',
-                success: myUserFunction,
-                data: {},
-                async: false
-
-            });
-        } else if (parm === stream) {
-            $.ajax({
-                type: 'GET',
-                url: 'https://wind-bow.hyperdev.space/twitch-api' + stream + user_name,
-                dataType: 'jsonp',
-                success: myStreamFunction,
-                data: {},
-                async: false
-
-            });
+            //If errors found...
+        } else {
+            //Extract user name from error message
+            nameError = json.message.split('\"');
+            nameError = nameError[1];
+            $('#' + nameError + ' .left-container .a-logo').addClass(nameError);
+            $('#' + nameError + ' img.logo').attr('src', logo);
+            $('#' + nameError + ' .left-container a.name').addClass(nameError);
+            $('#' + nameError + ' .left-container a.name').text(nameError);
+            $('#' + nameError + ' .left-container .bio').removeClass('hidden');
+            $('#' + nameError + ' .left-container .highlight').text('Error: ');
+            $('#' + nameError + ' .left-container .highlight').addClass('error');
+            $('#' + nameError + ' .left-container .bio').append(error);
         }
     }
-
-    for (var i = 0; i < usersList.length; i = i + 1) {
-        getData(user, usersList[i]);
+    //Handle JSON response for streams user online
+    function streams(json) {
+        var stream = json.stream,
+            name,
+            game;
+        //If user streaming...
+        if (stream) {
+            name = json.stream.channel.name;
+            game = json.stream.game;
+            $('#' + name).removeClass('offline');
+            $('#' + name).addClass('online');
+            $('#' + name + ' .stream-info').removeClass('hidden');
+            $('#' + name + ' .stream-info').append(game);
+            $('#' + name + ' .status-icon').removeClass('icon-offline');
+            $('#' + name + ' .status-icon').addClass('icon-online');
+            $('#' + name + ' .right-container').html('<span class="status-icon icon-online"></span>Online');
+        }
     }
-
-    for (var i = 0; i < usersList.length; i = i + 1) {
-        getData(stream, usersList[i]);
+    //Simple AJAX error handler, just in case
+    function errorHandler(xhr, ajaxOptions, thrownError) {
+        console.log('Error: ', xhr, ajaxOptions, thrownError);
     }
-
-
+    //Looping through all users and GET ajax response
+    for (i = 0; i < usersList.length; i = i + 1) {
+        $.ajax({
+            type: 'GET',
+            url: 'https://wind-bow.gomix.me/twitch-api/users/' + usersList[i],
+            dataType: 'jsonp',
+            error: errorHandler,
+            success: users,
+            async: false
+        });
+    }
+    //Looping through all streams and GET ajax response
+    for (j = 0; j < usersList.length; j = j + 1) {
+        $.ajax({
+            type: 'GET',
+            url: 'https://wind-bow.gomix.me/twitch-api/streams/' + usersList[j],
+            dataType: 'jsonp',
+            error: errorHandler,
+            success: streams,
+            async: false
+        });
+    }
+    //Filtering all/online/offline users
     $('#online').on('click', function () {
         $('.offline').addClass('hidden');
         $('.online').removeClass('hidden');
@@ -92,7 +125,6 @@ $(document).ready(function () {
         $('#online').parent().removeClass('active');
         $('#all').parent().removeClass('active');
     });
-
     $('#all').on('click', function () {
         $('.online').removeClass('hidden');
         $('.offline').removeClass('hidden');
@@ -100,24 +132,19 @@ $(document).ready(function () {
         $('#offline').parent().removeClass('active');
         $('#online').parent().removeClass('active');
     });
-
+    //Filtering search results
+    //Snippet taken and adjusted from here: https://php.quicoto.com/jquery-live-filter/
     $('#filter').keyup(function () {
-
         // Retrieve the input field text
         var filter = $(this).val();
-
         // Loop through the streamers list
         $('.name').each(function () {
-
             // If the list item does not contain the text phrase fade it out
             if ($(this).text().search(new RegExp(filter, 'i')) < 0) {
                 $(this).parent().parent().fadeOut();
-
             } else {
                 $(this).parent().parent().fadeIn();
             }
         });
-
     });
-
 });
